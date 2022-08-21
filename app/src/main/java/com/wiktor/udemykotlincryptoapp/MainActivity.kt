546bbd70d42f3fc8d00c3657c8a5768674ab0fdd
wiktor <wiktor@bk.ru>
2022-08-21 18:23:37 +0300

    lesson_41 init

--- app/src/main/java/com/wiktor/udemykotlincryptoapp/MainActivity.kt
+++ app/src/main/java/com/wiktor/udemykotlincryptoapp/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 
 
 //https://min-api.cryptocompare.com/
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
