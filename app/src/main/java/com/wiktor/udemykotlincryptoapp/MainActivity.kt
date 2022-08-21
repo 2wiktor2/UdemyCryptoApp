63cb13006b78751404ef9b21888d8a949ab97319
wiktor <wiktor@bk.ru>
2022-08-21 18:21:04 +0300

    Добавлены зависимости

--- app/src/main/java/com/wiktor/udemykotlincryptoapp/MainActivity.kt
+++ app/src/main/java/com/wiktor/udemykotlincryptoapp/MainActivity.kt
@@ -3,6 +3,9 @@ package com.wiktor.udemykotlincryptoapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+//https://min-api.cryptocompare.com/
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
